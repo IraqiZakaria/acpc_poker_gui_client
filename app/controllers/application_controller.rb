ea867ce19e607c1046491c9a6cb70c80738b96df
Justin Reese <justin.x.reese@gmail.com>
2014-12-20 09:08:48 -0500

    Adds user logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ require_relative '../../lib/application_defs'
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :layout
+  before_filter :authenticate_user!
 
   def self.report_error_request_message
     "please report this incident on the issue tracker, #{ApplicationDefs::ISSUE_TRACKER}"
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
